3bafeb47146c498c94ceee12de9f93ce51d23ddd
Giuseppe Pisano <giuseppe.pisano3@studio.unibo.it>
2021-11-17 17:58:44 +0100

    refactoring splitting principle

--- actor-solver/src/main/kotlin/it/unibo/tuprolog/argumentation/actor/actors/KbDistributor.kt
+++ actor-solver/src/main/kotlin/it/unibo/tuprolog/argumentation/actor/actors/KbDistributor.kt
@@ -5,9 +5,8 @@ import akka.actor.typed.javadsl.AbstractBehavior
 import akka.actor.typed.javadsl.ActorContext
 import akka.actor.typed.javadsl.Behaviors
 import akka.actor.typed.javadsl.Receive
-import akka.cluster.sharding.typed.javadsl.ClusterSharding
-import akka.cluster.sharding.typed.javadsl.EntityTypeKey
-import it.unibo.tuprolog.argumentation.actor.libs.TheoryChainer
+import it.unibo.tuprolog.argumentation.actor.actors.splitting.SplitKnowledgeBase
+import it.unibo.tuprolog.argumentation.actor.actors.splitting.SupportSplit
 import it.unibo.tuprolog.argumentation.actor.message.Add
 import it.unibo.tuprolog.argumentation.actor.message.Eval
 import it.unibo.tuprolog.argumentation.actor.message.EvalResponse
@@ -19,44 +18,31 @@ import it.unibo.tuprolog.argumentation.actor.message.Label
 import it.unibo.tuprolog.argumentation.actor.message.RequireEvaluation
 import it.unibo.tuprolog.argumentation.actor.message.Reset
 import it.unibo.tuprolog.argumentation.actor.message.Response
-import it.unibo.tuprolog.argumentation.core.Arg2pSolver
-import it.unibo.tuprolog.argumentation.core.dsl.arg2pScope
-import it.unibo.tuprolog.argumentation.core.libs.basic.FlagsBuilder
-import it.unibo.tuprolog.core.Struct
-import it.unibo.tuprolog.core.parsing.parse
-import it.unibo.tuprolog.solve.classic.ClassicSolverFactory
-import it.unibo.tuprolog.solve.flags.FlagStore
-import it.unibo.tuprolog.solve.flags.Unknown
-import it.unibo.tuprolog.theory.Theory
-import it.unibo.tuprolog.theory.parsing.parse
 import kotlin.random.Random
 
 class KbDistributor private constructor(context: ActorContext<KbMessage>) : AbstractBehavior<KbMessage>(context) {
 
-    private val workers: MutableList<HelpWorker> = mutableListOf()
+    private val kb: SplitKnowledgeBase = SplitKnowledgeBase()
     private val evaluationCache: MutableList<EvaluationCache> = mutableListOf()
 
     override fun createReceive(): Receive<KbMessage> =
         newReceiveBuilder()
             .onMessage(Reset::class.java) {
                 context.log.info("Resetting")
-                // workers.forEach {
-                //    context.stop(it.ref)
-                // }
-                workers.clear()
+                kb.reset()
                 evaluationCache.clear()
                 this
             }
             .onMessage(Add::class.java) { command ->
                 context.log.info("Adding ${command.elem}")
-                updateWorkers(command.elem)
+                SupportSplit.updateWorkers(command.elem, kb, context)
                 this
             }
             .onMessage(RequireEvaluation::class.java) { command ->
                 context.log.info("Eval for ${command.elem}")
-                val cache = EvaluationCache("eval_${Random.nextInt(0, Int.MAX_VALUE)}", command.elem, workers.size, command.replyTo)
+                val cache = EvaluationCache("eval_${Random.nextInt(0, Int.MAX_VALUE)}", command.elem, kb.splitsNumber(), command.replyTo)
                 evaluationCache.add(cache)
-                workers.forEach { it.ref.tell(Eval(cache.id, command.elem)) }
+                kb.broadcast(Eval(cache.id, command.elem))
                 this
             }
             .onMessage(EvalResponse::class.java) { command ->
@@ -66,77 +52,11 @@ class KbDistributor private constructor(context: ActorContext<KbMessage>) : Abst
                 }
                 this
             }.onMessage(FindAttacker::class.java) { command ->
-                command.replyTo.tell(ExpectedResponses(command.id, workers.count()))
-                workers.forEach { it.ref.tell(command) }
+                command.replyTo.tell(ExpectedResponses(command.id, kb.splitsNumber()))
+                kb.broadcast(command)
                 this
             }
             .build()
-
-    private fun solver() = ClassicSolverFactory.mutableSolverWithDefaultBuiltins(
-        otherLibraries = Arg2pSolver.default(listOf(FlagsBuilder().create()), listOf(TheoryChainer))
-            .to2pLibraries(),
-        flags = FlagStore.DEFAULT.set(Unknown, Unknown.FAIL)
-    )
-
-    private fun createSolver(rule: String) =
-        solver().also {
-            context.log.info("Creating $rule")
-            val id = "solver_${Random.nextInt()}"
-            val ref = ClusterSharding.get(context.system)
-                .entityRefFor(EntityTypeKey.create(KbMessage::class.java, "evaluator"), id)
-            val worker = HelpWorker(id, ref, it, mutableListOf(rule))
-            workers.add(worker)
-            it.assertA(Struct.parse(rule, it.operators))
-            arg2pScope { it.solve("context_reset" and ("parser" call "convertAllRules"(`_`))).first() }
-            worker.ref.tell(Add(rule))
-        }
-
-    private fun updateSolverKnowledge(worker: HelpWorker, rule: String) {
-        context.log.info("Updating $rule")
-        worker.theory.assertA(Struct.parse(rule, worker.theory.operators))
-        worker.rules.add(rule)
-        arg2pScope { worker.theory.solve("context_reset" and ("parser" call "convertAllRules"(`_`))).first() }
-        worker.ref.tell(Add(rule))
-    }
-
-    private fun mergeSolvers(oldWorkers: List<HelpWorker>, rule: String) {
-        context.log.info("Merging $rule")
-        workers.removeAll(oldWorkers)
-        val kb = oldWorkers.flatMap { it.rules }.toMutableList().also { it.add(rule) }
-        // oldWorkers.forEach { context.stop(it.ref) }
-        solver().also { solver ->
-            val id = "solver_${Random.nextInt()}"
-            val ref = ClusterSharding.get(context.system)
-                .entityRefFor(EntityTypeKey.create(KbMessage::class.java, "evaluator"), id)
-            val worker = HelpWorker(id, ref, solver, kb)
-            workers.add(worker)
-            kb.forEach {
-                worker.ref.tell(Add(it))
-                worker.theory.assertA(Struct.parse(it, worker.theory.operators))
-            }
-            arg2pScope { solver.solve("context_reset" and ("parser" call "convertAllRules"(`_`))).first() }
-        }
-    }
-
-    private fun updateWorkers(rule: String) {
-        val helperSolver = solver()
-        helperSolver.loadStaticKb(Theory.parse("$rule.", helperSolver.operators))
-        arg2pScope {
-            helperSolver.solve("parser" call "convertAllRules"(X))
-                .filter { it.isYes }
-                .map { it.substitution[X]!! }
-                .first().also { rules ->
-                    val selected = workers.filter { elem ->
-                        elem.theory.solve("chainer" call "chainable"(rules)).filter { it.isYes }.any()
-                    }
-                    when (selected.count()) {
-                        0 -> createSolver(rule)
-                        1 -> updateSolverKnowledge(selected.first(), rule)
-                        else -> mergeSolvers(selected, rule)
-                    }
-                }
-        }
-    }
 
     private fun evaluateResponses(cache: EvaluationCache) {
         if (cache.responses.size < cache.responseNumber) return
---
+++ actor-solver/src/main/kotlin/it/unibo/tuprolog/argumentation/actor/actors/splitting/SupportSplit.kt
@@ -0,0 +1,92 @@
+package it.unibo.tuprolog.argumentation.actor.actors.splitting
+
+import akka.actor.typed.javadsl.ActorContext
+import akka.cluster.sharding.typed.javadsl.ClusterSharding
+import akka.cluster.sharding.typed.javadsl.EntityTypeKey
+import it.unibo.tuprolog.argumentation.actor.actors.HelpWorker
+import it.unibo.tuprolog.argumentation.actor.libs.TheoryChainer
+import it.unibo.tuprolog.argumentation.actor.message.Add
+import it.unibo.tuprolog.argumentation.actor.message.KbMessage
+import it.unibo.tuprolog.argumentation.core.Arg2pSolver
+import it.unibo.tuprolog.argumentation.core.dsl.arg2pScope
+import it.unibo.tuprolog.argumentation.core.libs.basic.FlagsBuilder
+import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.parsing.parse
+import it.unibo.tuprolog.solve.classic.ClassicSolverFactory
+import it.unibo.tuprolog.solve.flags.FlagStore
+import it.unibo.tuprolog.solve.flags.Unknown
+import it.unibo.tuprolog.theory.Theory
+import it.unibo.tuprolog.theory.parsing.parse
+import kotlin.random.Random
+
+object SupportSplit : SplittingPrinciple {
+
+    override fun updateWorkers(rule: String, kb: SplitKnowledgeBase, context: ActorContext<KbMessage>) {
+        val helperSolver = solver()
+        helperSolver.loadStaticKb(Theory.parse("$rule.", helperSolver.operators))
+        arg2pScope {
+            helperSolver.solve("parser" call "convertAllRules"(X))
+                .filter { it.isYes }
+                .map { it.substitution[X]!! }
+                .first().also { rules ->
+                    val selected = kb.workers.filter { elem ->
+                        elem.theory.solve("chainer" call "chainable"(rules)).filter { it.isYes }.any()
+                    }
+                    when (selected.count()) {
+                        0 -> createSolver(rule, kb.workers, context)
+                        1 -> updateSolverKnowledge(rule, selected.first(), context)
+                        else -> mergeSolvers(rule, selected, kb.workers, context)
+                    }
+                }
+        }
+    }
+
+    private fun createWorker(rules: MutableList<String>, context: ActorContext<KbMessage>): HelpWorker =
+        solver().let { solver ->
+            val id = "solver_${Random.nextInt()}"
+            val ref = ClusterSharding.get(context.system)
+                .entityRefFor(EntityTypeKey.create(KbMessage::class.java, "evaluator"), id)
+            val worker = HelpWorker(id, ref, solver, rules)
+            rules.forEach {
+                worker.ref.tell(Add(it))
+                worker.theory.assertA(Struct.parse(it, worker.theory.operators))
+            }
+            arg2pScope { solver.solve("context_reset" and ("parser" call "convertAllRules"(`_`))).first() }
+            worker
+        }
+
+    private fun createSolver(rule: String, workers: MutableList<HelpWorker>, context: ActorContext<KbMessage>) {
+        context.log.info("Creating $rule")
+        workers.add(
+            createWorker(
+                mutableListOf(rule),
+                context
+            )
+        )
+    }
+
+    private fun updateSolverKnowledge(rule: String, worker: HelpWorker, context: ActorContext<KbMessage>) {
+        context.log.info("Updating $rule")
+        worker.theory.assertA(Struct.parse(rule, worker.theory.operators))
+        worker.rules.add(rule)
+        arg2pScope { worker.theory.solve("context_reset" and ("parser" call "convertAllRules"(`_`))).first() }
+        worker.ref.tell(Add(rule))
+    }
+
+    private fun mergeSolvers(rule: String, oldWorkers: List<HelpWorker>, workers: MutableList<HelpWorker>, context: ActorContext<KbMessage>) {
+        context.log.info("Merging $rule")
+        workers.removeAll(oldWorkers)
+        workers.add(
+            createWorker(
+                oldWorkers.flatMap { it.rules }.toMutableList().also { it.add(rule) },
+                context
+            )
+        )
+    }
+
+    private fun solver() = ClassicSolverFactory.mutableSolverWithDefaultBuiltins(
+        otherLibraries = Arg2pSolver.default(listOf(FlagsBuilder().create()), listOf(TheoryChainer))
+            .to2pLibraries(),
+        flags = FlagStore.DEFAULT.set(Unknown, Unknown.FAIL)
+    )
+}
