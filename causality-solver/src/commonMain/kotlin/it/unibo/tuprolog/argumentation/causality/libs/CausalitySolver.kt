ceaf7654ae107998ed4feeed5b57c262da37ee44
Giuseppe Pisano <giuseppe.pisano3@studio.unibo.it>
2024-10-02 11:13:43 +0200

    fix: causality solve

--- causality-solver/src/commonMain/kotlin/it/unibo/tuprolog/argumentation/causality/libs/CausalitySolver.kt
+++ causality-solver/src/commonMain/kotlin/it/unibo/tuprolog/argumentation/causality/libs/CausalitySolver.kt
@@ -14,7 +14,6 @@ import it.unibo.tuprolog.argumentation.core.model.Graph
 import it.unibo.tuprolog.argumentation.core.model.LabelledArgument
 import it.unibo.tuprolog.core.Clause
 import it.unibo.tuprolog.core.Struct
-import it.unibo.tuprolog.core.Substitution
 import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.core.operators.OperatorSet
 import it.unibo.tuprolog.core.parsing.parse
@@ -40,43 +39,73 @@ class CausalitySolver : ArgLibrary, Loadable {
             val cause: Term = request.arguments[0]
             val goal: Term = request.arguments[1]
 
-            fun checkCause(x: Argument, cause: String) : Boolean =
-                if (x.conclusion == cause) true
-                else x.supports.map { checkCause(it, cause) }.any()
+            fun checkCause(
+                x: Argument,
+                cause: String,
+            ): Boolean =
+                if (x.conclusion == cause) {
+                    true
+                } else {
+                    x.supports.map { checkCause(it, cause) }.any { it }
+                }
 
-            fun attackers(x: Argument, graph : Graph) : List<LabelledArgument> =
+            fun attackers(
+                x: Argument,
+                graph: Graph,
+            ): List<LabelledArgument> =
                 graph.attacks
                     .filter { it.target.identifier == x.identifier }
                     .map { arg -> graph.labellings.find { arg.attacker.identifier == it.argument.identifier }!! }
 
-            fun opponents(x: Argument, graph : Graph) : List<LabelledArgument> =
+            fun opponents(
+                x: Argument,
+                graph: Graph,
+            ): List<LabelledArgument> =
                 attackers(x, graph).let { a ->
                     a + a.flatMap { att -> attackers(att.argument, graph).flatMap { opponents(it.argument, graph) } }
                 }
 
-            fun supporters(x: Argument, graph : Graph) : List<LabelledArgument> =
+            fun supporters(
+                x: Argument,
+                graph: Graph,
+            ): List<LabelledArgument> =
                 graph.labellings
                     .filter { x.identifier == it.argument.identifier } +
-                        attackers(x, graph).flatMap {
-                            att -> attackers(att.argument, graph).flatMap { supporters(it.argument, graph) }
+                    attackers(x, graph).flatMap {
+                            att ->
+                        attackers(att.argument, graph).flatMap { supporters(it.argument, graph) }
+                    }
+
+            fun complement(t: Term): String =
+                t.toString()
+                    .replace(" ", "")
+                    .replace("'", "").let {
+                        if (it.startsWith("-")) {
+                            it.drop(1)
+                        } else {
+                            "-$it"
                         }
+                    }
 
-            fun complement(t: Term) : String =
-                t.toString().replace(" ", "").let {
-                    if (it.startsWith("-")) it.drop(1)
-                    else "-$it"
-                }
+            fun remove(
+                term: Term,
+                kb: Theory,
+            ): Theory =
+                Theory.of(
+                    kb.filterNot {
+                        Unificator.default.match(
+                            it.head!!.asTerm(),
+                            Struct.parse(":->(_, $term)"),
+                        )
+                    },
+                ).plus(Clause.of(Struct.parse(":->(fk${Random.nextUInt()}, ${complement(term)})")))
 
-            fun remove(term: Term, kb : Theory) : Theory =
-                Theory.of(kb.filterNot { Unificator.default.match(it.head!!.asTerm(),
-                    Struct.parse(":->(_, $term)")) }).plus(Clause.of(Struct.parse(":->(fk${Random.nextUInt()}, ${complement(term)})")))
-
-            fun solveFresh(kb: Theory) : Graph =
+            fun solveFresh(kb: Theory): Graph =
                 arg2pScope {
                     ClassicSolverFactory.mutableSolverWithDefaultBuiltins(
                         otherLibraries = Arg2pSolver.default(listOf(FlagsBuilder(graphExtensions = emptyList()).create())).to2pLibraries(),
                         flags = FlagStore.DEFAULT.set(Unknown, Unknown.FAIL).set(TrackVariables, TrackVariables.ON),
-                        staticKb = kb
+                        staticKb = kb,
                     ).let { solver ->
                         solver.solve(Struct.parse("buildLabelSetsSilent") and "context_active"(X))
                             .filter { it.isYes }
@@ -87,7 +116,7 @@ class CausalitySolver : ArgLibrary, Loadable {
                 }
 
             //  1a. check justifications (new semantics - temporarily use grounded), or
-            fun checkJustification(graph : Graph) : Boolean =
+            fun checkJustification(graph: Graph): Boolean =
                 graph.labellings.asSequence().filter { it.label == "in" && it.argument.conclusion == goal.toString() }
                     .flatMap { supporters(it.argument, graph) }
                     .filter { it.label == "in" }
@@ -95,16 +124,19 @@ class CausalitySolver : ArgLibrary, Loadable {
                     .any { it }
 
             //  2b. check if its intervention (graph built on rule base + negation) refutes the goal
-            fun checkRefutation(graph : Graph) : Boolean =
+            fun checkRefutation(graph: Graph): Boolean =
                 graph.labellings.asSequence().filter { it.label == "out" && it.argument.conclusion == goal.toString() }
                     .flatMap { opponents(it.argument, graph) }
                     .filter { it.label == "in" }
                     .map { checkCause(it.argument, complement(cause)) }
                     .any { it }
 
-            return sequenceOf(request.replyWith(
-                checkJustification(solveFresh(request.context.staticKb)) ||
-                        checkRefutation(solveFresh(remove(cause, request.context.staticKb)))))
+            return sequenceOf(
+                request.replyWith(
+                    checkJustification(solveFresh(request.context.staticKb)) ||
+                        checkRefutation(solveFresh(remove(cause, request.context.staticKb))),
+                ),
+            )
         }
     }
 
--- causality-solver/src/commonTest/kotlin/it/unibo/tuprolog/argumentation/causality/BasicTest.kt
+++ causality-solver/src/commonTest/kotlin/it/unibo/tuprolog/argumentation/causality/BasicTest.kt
@@ -2,7 +2,6 @@ package it.unibo.tuprolog.argumentation.causality
 
 import it.unibo.tuprolog.argumentation.core.Arg2pSolver
 import it.unibo.tuprolog.argumentation.core.dsl.arg2pScope
-import it.unibo.tuprolog.argumentation.core.libs.basic.FlagsBuilder
 import it.unibo.tuprolog.solve.classic.ClassicSolverFactory
 import it.unibo.tuprolog.solve.flags.FlagStore
 import it.unibo.tuprolog.solve.flags.TrackVariables
@@ -14,31 +13,29 @@ import kotlin.test.assertEquals
 
 class BasicTest {
     @Test
-
     fun causalityTest() {
-
-        val theory = """
+        val theory =
+            """
             tx2 : d => undercut(r2).
             
             r1 :-> a.
             r2 : a => b.
             
-        """.trimIndent()
+            """.trimIndent()
 
         arg2pScope {
-
             ClassicSolverFactory.mutableSolverWithDefaultBuiltins(
                 otherLibraries = Arg2pSolver.causality().to2pLibraries(),
                 flags = FlagStore.DEFAULT.set(Unknown, Unknown.FAIL).set(TrackVariables, TrackVariables.ON),
-                staticKb = Theory.parse(theory, Arg2pSolver.default().operators())
+                staticKb = Theory.parse(theory, Arg2pSolver.default().operators()),
             ).also {
-//                val a = it.solve("solve"("a", "b")).first()
-//                assertEquals(a.isYes, true)
-//                val b = it.solve("solve"("b", "a")).first()
-//                assertEquals(b.isYes, false)
-//                val c = it.solve("solve"("d", "a")).first()
-//                assertEquals(c.isYes, false)
-                val d = it.solve("solve"("-d", "a")).first()
+                val a = it.solve("solve"("a", "b")).first()
+                assertEquals(a.isYes, true)
+                val b = it.solve("solve"("b", "a")).first()
+                assertEquals(b.isYes, false)
+                val c = it.solve("solve"("d", "b")).first()
+                assertEquals(c.isYes, false)
+                val d = it.solve("solve"("-d", "b")).first()
                 assertEquals(d.isYes, true)
             }
         }
